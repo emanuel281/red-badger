8e11ab344e23638aa2a453ee80b0e084aaeb29b5
Emanuel (Sne) <emanuel281@users.noreply.github.com>
2025-11-05 01:14:32 +0200

    Robot path are plotted correctly.
    Signed-off-by: Emanuel (Sne) <emanuel281@users.noreply.github.com>

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 ## Solution
 * Since this can be thought as just graphing problem, we can use matplotlib to solve this problem
 * First we create a plain figure (grid class)
-** Initialize the grid with given width and height
+** Initialize the grid with 50x50 width and height
 ** Create a function that will be used add robot to the grid
-** Create a function that will be used to show the grid
+** Create a function that will be used to show robots on the grid
+** Create helper functions to convert direction to coordinates
 * Then we create a robot class:
 ** Initialize the robot with given grid, starting position, direction and steps
 ** Create a function that will be used to move the robot on the grid (turn and move)
--- red_badger/main.py
+++ red_badger/main.py
@@ -1,36 +1,51 @@
 import matplotlib.pyplot as plt
-import numpy as np
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Literal, Tuple
 
 
 class Robot:
-    def __init__(self, starting_x: int, starting_y: int, direction: str):
+    def __init__(self, starting_position: Tuple[int, int], grid_size: Tuple[int, int], direction: Literal["N", "S", "E", "W"]):
         """Initialize the robot with given grid and starting position
         
         Args:
             grid (Grid): The grid the robot is on
-            starting_x (int): The starting x position of the robot
-            starting_y (int): The starting y position of the robot
+            starting_position (Tuple[int, int]): The starting position of the robot
             direction (str): The direction the robot is facing
         """
-        self.x = starting_x
-        self.y = starting_y
+        self.x = starting_position[0]
+        self.y = starting_position[1]
         self.direction = direction
-        self.moves: List[Tuple[int, int]] = [(starting_x, starting_y)]
+        self.moves: List[Tuple[int, int]] = [(self.x, self.y)]
+        self.grid_size = grid_size
+        self.is_lost = False
 
-    def _get_new_direction(self, direction: str) -> str:
+    def check_if_lost(self, next_move: Tuple[int, int]):
+        """Check if the robot is lost
+        
+        Args:
+            next_move (Tuple[int, int]): The next position of the robot
+        """
+        x_max = self.grid_size[0]
+        y_max = self.grid_size[1]
+        next_x = next_move[0]
+        next_y = next_move[1]
+        if next_x < 0 or next_x > x_max or next_y < 0 or next_y > y_max or self.is_lost:
+            self.is_lost = True
+            return True
+        return False
+
+    def _update_direction(self, instruction: Literal["L", "R"]) -> str:
         """
             Change the robot direction by 90 degrees.
 
         
         Args:
-            direction (str): The direction to turn the robot to.
+            instruction (str): Instruction to turn the robot to.
             
         Returns:
             str: The new direction of the robot
         """
         new_direction = self.direction
-        match direction:
+        match instruction:
             case "L":
                 if self.direction == "N":
                     new_direction = "W"
@@ -49,7 +64,29 @@ class Robot:
                     new_direction = "S"
                 elif self.direction == "W":
                     new_direction = "N"
+        self.direction = new_direction
         return new_direction
+
+    def _get_new_position(self, direction: str, pace: int = 1) -> Tuple[int, int]:
+        """
+            Get the new position of the robot
+        
+        Args:
+            direction (str): The direction to move the robot to.
+            pace (int, optional): The number of steps to move the robot. Defaults to 1.
+        
+        Returns:
+            Tuple[int, int]: The new position of the robot
+        """
+        match direction:
+            case "N":
+                return (self.x, self.y + pace)
+            case "S":
+                return (self.x, self.y - pace)
+            case "E":
+                return (self.x + pace, self.y)
+            case "W":
+                return (self.x - pace, self.y)
 
     def move(self, moves: Iterable[str], pace: int = 1):
         """Move the robot in the direction it is facing
@@ -58,22 +95,22 @@ class Robot:
             moves (Iterable[str]): The moves to make the robot
             pace (int, optional): The number of steps to move the robot. Defaults to 1.
         """
-        for move in moves:
+        moves_len = min(100, len(moves))
+        for i in range(moves_len):
+            if self.is_lost:
+                break
+            move = moves[i]
             if move == "L" or move == "R":
-                self.direction = self._get_new_direction(move)
+                self._update_direction(move)
             elif move == "F":
-                if self.direction == "N":
-                    self.y += pace
-                    self.moves.append((self.x, self.y))
-                elif self.direction == "S":
-                    self.y -= pace
-                    self.moves.append((self.x, self.y))
-                elif self.direction == "E":
-                    self.x += pace
-                    self.moves.append((self.x, self.y))
-                elif self.direction == "W":
-                    self.x -= pace
-                    self.moves.append((self.x, self.y))
+                next_position = self._get_new_position(self.direction, pace)
+                if self.check_if_lost(next_position):
+                    break
+                self.x, self.y = next_position
+                self.moves.append(next_position)
+
+    def display_latest_position(self):
+        print(f"{self.x} {self.y} {self.direction} {'LOST' if self.is_lost else ''}")
 
 
 class Grid:
@@ -83,10 +120,6 @@ class Grid:
         self.fig, self.ax = plt.subplots()
         self.robots: List[Robot] = []
     
-    def test_graphing(self):
-        self.ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
-        self.ax.plot([1, 2, 3, 4], [1, 40, 20, 30])
-    
     def add_robot(self, robot: Robot):
         self.robots.append(robot)
 
@@ -95,14 +128,14 @@ class Grid:
         for robot in self.robots:
             plots.append(robot.moves)
         self.plots = plots
-    
-    def display_final_positions(self):
+
+    def display_latest_positions(self):
         for robot in self.robots:
-            print(f"{robot.x} {robot.y} {robot.direction}")
+            robot.display_latest_position()
 
     def show(self):
         self.save_all_moves()
-        self.display_final_positions()
+        self.display_latest_positions()
         for plot in self.plots:
             x_axis, y_axis = zip(*plot)
             self.ax.plot(x_axis, y_axis, marker="x")
@@ -121,13 +154,13 @@ if __name__ == "__main__":
         LLFFFLFLFL
     """
     grid = Grid()
-    robot1 = Robot(1, 1, "E")
+    robot1 = Robot((1, 1), (5, 3), "E")
     robot1.move("RFRFRFRF")
     grid.add_robot(robot1)
-    robot2 = Robot(3, 2, "N")
+    robot2 = Robot((3, 2), (5, 3), "N")
     robot2.move("FRRFLLFFRRFLL")
     grid.add_robot(robot2)
-    robot3 = Robot(0, 3, "W")
+    robot3 = Robot((0, 3), (5, 3), "W")
     robot3.move("LLFFFLFLFL")
     grid.add_robot(robot3)
     grid.show()
